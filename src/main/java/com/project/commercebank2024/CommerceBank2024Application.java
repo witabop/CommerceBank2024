b994255805ef6e063b1dbdcd4ef8cc4657353282
witabop <108284606+witabop@users.noreply.github.com>
2024-01-24 17:09:42 -0600

    Initial Commit

---
+++ src/main/java/com/project/commercebank2024/CommerceBank2024Application.java
@@ -0,0 +1,13 @@
+package com.project.commercebank2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceBank2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceBank2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/commercebank2024/CommerceBank2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.commercebank2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceBank2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
